2ccb203704a7625196f3edbc8bf8090d2639e015
karnikamit <karnikamit46@gmail.com>
2026-04-04 16:02:42 +0530

    init

---
+++ src/main/java/com/karnikamit/school_admission_system/SchoolAdmissionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.karnikamit.school_admission_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAdmissionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAdmissionSystemApplication.class, args);
+	}
+
+}
