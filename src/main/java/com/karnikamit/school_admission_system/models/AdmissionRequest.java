0e14b36020f2123b0f996e7c7938f8caff2e4b1a
Amitkumar Karnik <karnikamit46@gmail.com>
2026-04-23 11:12:22 +0530

    Dev/karnik/init props (#2)
    * add app props and init controller
    
    * sample admission-request without processing
    
    * save request to Postgres and send saved response to kafka

--- src/main/java/com/karnikamit/school_admission_system/models/AdmissionRequest.java
+++ src/main/java/com/karnikamit/school_admission_system/models/AdmissionRequest.java
@@ -1,40 +1,25 @@
 package com.karnikamit.school_admission_system.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="admissions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdmissionRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private int age;
     private String address;
+
+    @Column(name = "uniform_required")
     private boolean uniformRequired;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public boolean isUniformRequired() {
-        return uniformRequired;
-    }
-
-    public void setUniformRequired(boolean uniformRequired) {
-        this.uniformRequired = uniformRequired;
-    }
 }
---
+++ src/main/java/com/karnikamit/school_admission_system/repositories/AdmissionRepository.java
@@ -0,0 +1,10 @@
+package com.karnikamit.school_admission_system.repositories;
+
+import com.karnikamit.school_admission_system.models.AdmissionRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdmissionRepository extends JpaRepository<AdmissionRequest, Long> {
+
+}
