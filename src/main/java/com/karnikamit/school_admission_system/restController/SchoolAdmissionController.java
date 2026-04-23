673043322520038188beb703a65e81244ccfafba
karnikamit <karnikamit46@gmail.com>
2026-04-23 10:33:48 +0530

    save request to Postgres and send saved response to kafka

--- src/main/java/com/karnikamit/school_admission_system/restController/SchoolAdmissionController.java
+++ src/main/java/com/karnikamit/school_admission_system/restController/SchoolAdmissionController.java
@@ -2,10 +2,14 @@ package com.karnikamit.school_admission_system.restController;
 
 import com.karnikamit.school_admission_system.models.AdmissionRequest;
 import com.karnikamit.school_admission_system.models.ApiResponse;
+import com.karnikamit.school_admission_system.repositories.AdmissionRepository;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -16,19 +20,34 @@ public class SchoolAdmissionController {
 
     private static final Logger logger = LoggerFactory.getLogger(SchoolAdmissionController.class);
 
+    @Autowired
+    private AdmissionRepository admissionRepository;
+
+    @Autowired
+    private KafkaTemplate<String, AdmissionRequest> kafkaTemplate;
+
+    private static final String TOPIC = "admission_requests";
+
+    @Transactional
     @PostMapping("/admission-request")
     public ResponseEntity<ApiResponse<String>> handleAdmissionRequest(@RequestBody AdmissionRequest admissionRequest) {
-        logger.info("Received admission request: {}", admissionRequest.toString());
-
+        logger.info("Received admission request for: {}", admissionRequest);
         try{
             // Logic to process admission (e.g., saving to DB)
-            String admissionId = "ADM -" + UUID.randomUUID().toString().substring(0, 8);
+            String admissionId = "ADM-" + UUID.randomUUID().toString().substring(0, 8);
 
             ApiResponse<String> response = new ApiResponse<>(
                     "Admission request submitted successfully!",
                     true,
                     admissionId
             );
+
+            // 1. Save to Postgres
+            AdmissionRequest savedRequest = admissionRepository.save(admissionRequest);
+
+            // 2. Send the saved object (with its new ID) to Kafka
+            kafkaTemplate.send(TOPIC, savedRequest);
+
             // Process the admission request and return a response
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (Exception e) {
