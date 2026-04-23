764398975b4a3300dbd9641ef6bf1e7f01ddd159
Amitkumar Karnik <karnikamit46@gmail.com>
2026-04-23 12:01:05 +0530

    fix docker.yml file (#3)

--- src/main/java/com/karnikamit/school_admission_system/SchoolAdmissionSystemApplication.java
+++ src/main/java/com/karnikamit/school_admission_system/SchoolAdmissionSystemApplication.java
@@ -2,8 +2,10 @@ package com.karnikamit.school_admission_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.karnikamit.school_admission_system")
 public class SchoolAdmissionSystemApplication {
 
 	public static void main(String[] args) {
