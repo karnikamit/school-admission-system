f17dfa229611361d8f74ec0d18558b0b5f32db55
Amitkumar Karnik <karnikamit46@gmail.com>
2026-04-24 12:25:54 +0530

    save to db pub to kafka (#4)
    * fix docker.yml file
    
    * save to DB and publish saved msg to kafka

--- src/main/java/com/karnikamit/school_admission_system/kafka/KafkaConfig.java
+++ src/main/java/com/karnikamit/school_admission_system/kafka/KafkaConfig.java
@@ -1,11 +1,20 @@
 package com.karnikamit.school_admission_system.kafka;
 
+import com.karnikamit.school_admission_system.models.AdmissionRequest;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
@@ -38,4 +47,17 @@ public class KafkaConfig {
                 .replicas(1)
                 .build();
     }
+
+    @Bean
+    public ProducerFactory<String, AdmissionRequest> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, AdmissionRequest> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
 }
